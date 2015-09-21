4f0e1183db3a74b7226a5d647e3a76c9f6f9b9fb
Matt Johnson <mjohn23@gmail.com>
2015-09-21 09:43:31 -0500

    Get first field of first row of csv

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -2,7 +2,14 @@ import csv, sys
 
 # Check if csv file is formatted right
 def check_file(file_name):
-    print file_name
+    csv_list = []
+    with open(file_name, 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            csv_list.append(row)
+
+    print csv_list[0][0]
+    
 
 file_name = sys.argv[1]
 check_file(file_name);
