7f1b8389e1768c866427434c4ec004b7ee7d837e
Matt Johnson <mjohn23@gmail.com>
2015-09-28 10:14:44 -0500

    Add bulk conversion of bad csv

--- convert_to_csv.sh
+++ convert_to_csv.sh
@@ -4,5 +4,6 @@ do
     echo "Processing $f"
     filename=$(basename "$f")
     filename="${filename%.*}"
-    in2csv $f > $(dirname $f)/$filename.csv
+    in2csv $f > $(dirname $f)/$filename.csv # convert xls to csv
+    env/bin/python csv_cleaner.py $(dirname $f)/$filename.csv # fix bad csv
 done
