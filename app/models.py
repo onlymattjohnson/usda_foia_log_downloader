fd68341b3ea00ef36d00ffb881d3404a4c224299
Matt Johnson <mjohn23@gmail.com>
2015-10-07 10:43:12 -0500

    Add first model

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Request(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    case_no = db.Column(db.String(32), index=True, unique=True)
+    date_received = db.Column(db.DateTime)
+    date_due = db.Column(db.DateTime, index=True)
+    date_closed = db.Column(db.DateTime, index=True)
+    track = db.Column(db.String(32)
+    subject = db.Column(db.String(500))
+
+    def __repr__(self):
+        return '<Case %r> %r' % (self.case_no, self.subject)    
