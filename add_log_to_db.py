6f41ad61f40a09c131365c072adadae1bc125d86
Matt Johnson <mjohn23@gmail.com>
2015-10-13 09:59:14 -0500

    Begin to read CSV files

--- add_log_to_db.py
+++ add_log_to_db.py
@@ -1,4 +1,20 @@
 #!env/bin/python
-import sys
+import csv, sys
 
-print sys.argv[1]
+def get_file_contents(file_name):
+    result = []
+    with open(file_name, 'rb') as csvfile:
+        filereader = csv.reader(csvfile)
+        for row in filereader:
+            result.append(row)
+
+    return result
+    
+
+try:
+    file_name = sys.argv[1]
+except:
+    print "You must give an argument for the name of the file"
+    sys.exit(1)
+
+print get_file_contents(file_name)
